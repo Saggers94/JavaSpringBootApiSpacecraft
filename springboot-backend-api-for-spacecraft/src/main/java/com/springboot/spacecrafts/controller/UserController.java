56b172164b6a59405c00f6a12b1d1a07003cc12b
saggers <sagarparmar0233@gmail.com>
2020-12-30 17:16:55 -0800

    added post request method for the users

--- springboot-backend-api-for-spacecraft/src/main/java/com/springboot/spacecrafts/controller/UserController.java
+++ springboot-backend-api-for-spacecraft/src/main/java/com/springboot/spacecrafts/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,11 @@ public class UserController {
 	@GetMapping("/users/{id}")
 	public Optional<User> one(@PathVariable Long id){
 		return userRepository.findById(id);
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user ) {
+		return userRepository.save(user);
 	}
 	
 }
